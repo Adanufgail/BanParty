a7ea020af4510052e16231bc8b0d048a1eaec6d3
Adanufgail <1276110+Adanufgail@users.noreply.github.com>
2018-11-21 14:00:27 -0600

    Pull IDs of followers of everyone in banlist.csv working

--- banparty.py
+++ banparty.py
@@ -8,6 +8,7 @@ import os
 import twitter
 from dateutil.parser import parse
 import creds
+from collections import OrderedDict
 
 __author__ = "Michael Lubert"
 __version__ = "0.1"
@@ -18,8 +19,8 @@ consumer_secret = creds.apikeys['consumer_secret']
 access_token_key = creds.apikeys['access_key']
 access_token_secret = creds.apikeys['access_secret']
 
-debug = False
-WAIT = 3
+debug = True
+WAIT = 15
 
 def countdown(t):
     while t > 0:
@@ -27,27 +28,76 @@ def countdown(t):
         t -= 1
         sys.stdout.flush()
         time.sleep(1)
+    sys.stdout.write('\r{}     ')
+
+def uniq(list):
+  last = object()
+  for item in list:
+    if item == last:
+      continue
+    yield item
+    last = item
+
+def sorter(l):
+  return list(uniq(sorted(l)))
 
 def load(api):
+    IDLIST=[]
+    if debug == True:
+      print "loading banlist.csv\n"
     with open("banlist.csv") as file:
+      if debug == True:
+        print "loaded. loading banids.csv\n"
+      with open("banids.csv") as ids:
+        if debug == True:
+          print "loaded. reading lines.\n"
+        for row in csv.reader(ids):
+          IDLIST.append(str(row[0]))
+        if debug == True:
+          print "read.\n"
         count = 0
         skiplike = False
         for row in csv.reader(file):
           user_name = str(row[0])
-          cursorp = "-1"
+          print "Finding followers of "+user_name
+          cursorn = "-1"
           try:
-            USERS=api.GetFollowersPaged(screen_name=user_name,cursor=cursorp,count=200)
-            for user in USERS:
-              try:
-                print user
-              except twitter.TwitterError, err:
-                print "Exception: %s\n" % err.message
+            while cursorn != 0: 
+              USERS=api.GetFollowerIDsPaged(screen_name=user_name,cursor=cursorn,count=5000)
+              cursorn=int(USERS[0])
+              if debug == True:
+                print "NEXT CURSOR: "+str(cursorn)+"\n"
+                print "PREV CURSOR: "+str(USERS[1])+"\n"
+              for user in USERS[2]:
+                try:
+                  if debug == True:
+                    print str(user)
+                  IDLIST.append(str(user))
+                except twitter.TwitterError, err:
+                  print "Exception: %s\n" % err.message
+              countdown(WAIT)
           except twitter.TwitterError, err:
             print "Exception: %s\n" % err.message
+    if debug == True:
+      print "\nFinished loading all user IDs. Sorting them.\n"
+    IDSET = sorter(IDLIST)
+    if debug == True:
+      print "\nSorting Complete. Opening banids.csv for writing.\n"
+
+    with open("banids.csv","w") as ids:
+      if debug == True:
+        print "\nFile open.\n"
+      for X in IDSET:
+        ids.write(str(X)+"\n")
+        if debug == True:
+          print(str(X))
+    if debug == True:
+      print "\writing complete.\n"
+      
 
 
 def delete(api):
-    with open("like2.js") as file:
+    with open("banids.csv") as file:
         count = 0
         skiplike = False
 
