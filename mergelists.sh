59edd85c7d43ca2ad98d6910023f2e4a7584183d
Adanufgail <adanufgail@gmail.com>
2019-01-04 22:29:27 +0000

    2019-01-04

--- mergelists.sh
+++ mergelists.sh
@@ -1,1 +1,1 @@
-wc -l banids.csv*; cat banids.csv banids.csv.temp | sort | uniq > banids2.csv; mv banids2.csv banids.csv; wc -l banids.csv
+wc -l banids.csv banids.csv.temp; cat banids.csv banids.csv.temp | sort | uniq > banids2.csv; mv banids2.csv banids.csv; wc -l banids.csv
