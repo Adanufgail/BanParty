42904449f00cb0f47d479a6d8d7fd092f916acfb
Adanufgail <adanufgail@gmail.com>
2019-02-18 20:50:27 +0000

    2019-02-18

--- compare.sh
+++ compare.sh
@@ -1,14 +1,14 @@
+#echo "making backup of banids.csv"
+#nice -n 20 tar cvf - banids.csv |nice -n 15 xz -v -7 > banids.$(date +%Y-%m-%d.%H-%M).csv.gz
 echo "combining banids.csv and banids.csv.temp"
-cat banids.csv banids.csv.temp > banidscombined.csv
-echo "making backup of banids.csv"
-mv banids.csv banids.$(date +%Y-%m-%d.%H-%M).csv
+cat banids.csv.temp >> banids.csv
 echo "splitting files"
-split --verbose -d -l 1000000 banidscombined.csv banids
-rm banidscombined.csv
-for X in $(ls banids[0-9]*)
+nice -n 19 split --verbose -a 4 -d -l 1000000 banids.csv tempfile
+mv banids.csv banids.$(date +%Y-%m-%d.%H-%M).csv
+for X in $(ls tempfile[0-9]*)
 do
   echo $X
-  for Y in $(ls banids[0-9]*)
+  for Y in $(ls tempfile[0-9]*)
   do
     echo $Y
     if [[ "x$X" = "x$Y" ]]
@@ -16,10 +16,10 @@ do
       echo same, skipping
     else
       echo sorting and uniq $X and $Y
-      cat $X $Y | sort | uniq > $X.new
+      cat $X $Y | nice -n 19 sort -u > $X.new
       sleep 2
       echo removing $Y from combo
-      cat $X $X.new | sort | uniq -d > $X.new2
+      cat $X $X.new | nice -n 19 sort -u > $X.new2
       sleep 2
       echo cleanup
       mv $X.new2 $X
